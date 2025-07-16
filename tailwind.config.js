6c23f210e4b24470eb03af0326c614321b267bc8
Shadabdullah <shadabdullah101@gmail.com>
2025-07-16 16:52:05 +0530

    Removed styles css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"], // Scan these files for classes
   important: '#zentask-container', // Ensures styles work in Obsidian's shadow DOM
-  // corePlugins: {
-  //   preflight: false, // Disable default CSS reset (avoids Obsidian conflicts)
-  // },
+  corePlugins: {
+    preflight: false, // Disable default CSS reset (avoids Obsidian conflicts)
+  },
   theme: {
     extend: {},
   },
